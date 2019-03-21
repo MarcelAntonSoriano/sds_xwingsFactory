f00156f0e2453e6403b650c64a41dcb817251025
SamuGB <43610968+SamuGB@users.noreply.github.com>
2019-03-21 15:45:44 +0100

    escandalo

--- XWingsFactorySoftware/Views/OtrasGestiones/escandallo_form.Designer.cs
+++ XWingsFactorySoftware/Views/OtrasGestiones/escandallo_form.Designer.cs
@@ -28,43 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.custom_treeview1 = new XWings_MainWindows.CustomControls.custom_treeview();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBox_Code = new System.Windows.Forms.TextBox();
+            this.txtBox_Desc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // custom_treeview1
+            // treeView
             // 
-            this.custom_treeview1.Location = new System.Drawing.Point(12, 12);
-            this.custom_treeview1.Name = "custom_treeview1";
-            this.custom_treeview1.SelectedItem = null;
-            this.custom_treeview1.Size = new System.Drawing.Size(334, 414);
-            this.custom_treeview1.TabIndex = 0;
+            this.treeView.Location = new System.Drawing.Point(12, 108);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(330, 335);
+            this.treeView.TabIndex = 0;
+            this.treeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_NodeMouseClick);
             // 
-            // textBox1
+            // groupBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(433, 88);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(112, 20);
-            this.textBox1.TabIndex = 1;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtBox_Desc);
+            this.groupBox1.Controls.Add(this.txtBox_Code);
+            this.groupBox1.Location = new System.Drawing.Point(348, 108);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(638, 335);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Producto Seleccionado";
+            // 
+            // txtBox_Code
+            // 
+            this.txtBox_Code.Location = new System.Drawing.Point(61, 46);
+            this.txtBox_Code.Name = "txtBox_Code";
+            this.txtBox_Code.Size = new System.Drawing.Size(107, 20);
+            this.txtBox_Code.TabIndex = 0;
+            // 
+            // txtBox_Desc
+            // 
+            this.txtBox_Desc.Location = new System.Drawing.Point(271, 46);
+            this.txtBox_Desc.Name = "txtBox_Desc";
+            this.txtBox_Desc.Size = new System.Drawing.Size(181, 20);
+            this.txtBox_Desc.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Descripción";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(18, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // escandallo_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.custom_treeview1);
+            this.ClientSize = new System.Drawing.Size(1009, 574);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.treeView);
             this.Name = "escandallo_form";
-            this.Text = "escandallo";
+            this.Text = "Escandallo";
             this.Load += new System.EventHandler(this.escandallo_form_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private CustomControls.custom_treeview custom_treeview1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox_Desc;
+        private System.Windows.Forms.TextBox txtBox_Code;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- XWingsFactorySoftware/Views/OtrasGestiones/escandallo_form.cs
+++ XWingsFactorySoftware/Views/OtrasGestiones/escandallo_form.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Collections.Generic;
 using XWingsFactorySoftware.Models;
 
 
@@ -10,6 +10,7 @@ namespace XWings_MainWindows.OtrasGestiones
     public partial class escandallo_form : Form
     {
         public xwingsfactoryEntities ef = new xwingsfactoryEntities();
+        private IDictionary<string, IEnumerable<string>> dict_escandallo;
         public escandallo_form()
         {
             InitializeComponent();
@@ -17,27 +18,66 @@ namespace XWings_MainWindows.OtrasGestiones
 
         private void escandallo_form_Load(object sender, EventArgs e)
         {
-            List<string> lista = new List<string>();
-
-            var producto_final = from b in ef.References.AsEnumerable()
-                                 where b.idReferenceType == 11
-                                 select b.codeReference;
-
-            var producto_intermedio = from b in ef.References.AsEnumerable()
-                                      where b.idReferenceType == 12
-                                      select b.codeReference;
-
-            foreach(var value in producto_intermedio)
+            CargarForm();
+        }
+        private void CargarForm()
+        {
+            dict_escandallo = ObtenerDatosEscandallo();
+            if (dict_escandallo != null)
             {
-                lista.Add(value.ToString());
+                TreeNode nave = new TreeNode("Nave XWing");
+                treeView.Nodes.Add(nave);
+
+                foreach (var elemento in dict_escandallo)
+                {
+                    TreeNode item = new TreeNode(elemento.Key);
+                    treeView.Nodes.Add(item);
+                    foreach (string value in elemento.Value)
+                        item.Nodes.Add(value);
+                }
             }
-
-            foreach (var value in producto_final) {
-                custom_treeview1.dict.Add(value.ToString(),lista);
+        }
+        private IDictionary<string, IEnumerable<string>> ObtenerDatosEscandallo()
+        {
+            IDictionary<string, IEnumerable<string>> dict_final = new Dictionary<string, IEnumerable<string>>();
+            try
+            {
+                IEnumerable<string> producto_intermedio_ids = from b in ef.Structure.AsEnumerable()
+                                                              where b.idReferenceFinal == 21
+                                                              select b.idReferencePart.ToString();
+                foreach (string id in producto_intermedio_ids)
+                {
+                    string parte_intermedia = (from a in ef.References
+                                               where a.idReference.ToString() == id.ToString()
+                                               select a.descReference).ToArray()[0];
+                    IEnumerable<string> producto_primo = from c in ef.Structure.AsEnumerable()
+                                                         join d in ef.References.AsEnumerable()
+                                                         on c.idReferencePart equals d.idReference
+                                                         where c.idReferenceFinal.ToString() == id
+                                                         select d.descReference.ToString();
+                    dict_final.Add(parte_intermedia, producto_primo);
+                }
             }
+            catch
+            {
+                dict_final = null;
+                MessageBox.Show("Error al Obtener los Datos de la Estructura");
+            }
+            return dict_final;
+        }
 
-
-            textBox1.Text = custom_treeview1.SelectedItem;
+        private void treeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            string selected_item = e.Node.Text;
+            string code = (from b in ef.References
+                           where b.descReference.ToString() == selected_item
+                           select b.codeReference).ToArray()[0];
+            string photo_path = (from b in ef.References
+                                where b.descReference.ToString() == selected_item
+                                select b.Photo).ToArray()[0];
+            txtBox_Code.Text = code;
+            txtBox_Desc.Text = e.Node.Text;
+            pictureBox1.ImageLocation = photo_path;
         }
     }
 }
