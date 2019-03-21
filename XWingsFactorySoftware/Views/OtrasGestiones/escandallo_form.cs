04b83740a086c1d0d73cb890fe36f17fd2586379
AlbertCouto <aguetta12@gmail.com>
2019-03-21 17:44:32 +0100

    wwaw

--- XWingsFactorySoftware/Views/OtrasGestiones/escandallo_form.cs
+++ XWingsFactorySoftware/Views/OtrasGestiones/escandallo_form.cs
@@ -10,6 +10,7 @@ namespace XWings_MainWindows.OtrasGestiones
     public partial class escandallo_form : Form
     {
         public xwingsfactoryEntities ef = new xwingsfactoryEntities();
+        public string idEscandallo { get; set; }
         private IDictionary<string, IEnumerable<string>> dict_escandallo;
         public escandallo_form()
         {
@@ -75,11 +76,64 @@ namespace XWings_MainWindows.OtrasGestiones
             string photo_path = (from b in ef.References
                                 where b.descReference.ToString() == selected_item
                                 select b.Photo).ToArray()[0];
+
             txtBox_Code.Text = code;
             txtBox_Desc.Text = e.Node.Text;
             pictureBox1.ImageLocation = photo_path;
+
+            string idH = (from b in ef.References
+                          where b.descReference == selected_item
+                          select b.idReference).ToArray()[0].ToString();
+
+
+            idEscandallo = idH.ToString();
+
+
+            string tipo = (from c in ef.References.AsEnumerable()
+                          join d in ef.ReferenceTypes.AsEnumerable()
+                          on c.idReferenceType equals d.idReferenceType
+                          where  d.idReferenceType.ToString() == (from x in ef.References
+                                                   where x.idReference.ToString() == idEscandallo
+                                                   select x.idReferenceType).ToArray()[0].ToString()
+                          select d.codeReferenceType).ToArray()[0];
+
+            if (tipo == "FINAL") RellenarListBoxFinal();
+            else RellenarListBoxMedio();
         }
 
+        private void RellenarListBoxFinal()
+        {
+            IEnumerable<string> producto_intermedio_ids = from b in ef.Structure.AsEnumerable()
+                                                          where b.idReferenceFinal.ToString() == idEscandallo
+                                                          select b.idReferencePart.ToString();
+
+            foreach (string id in producto_intermedio_ids)
+            {
+                string parte_intermedia = (from a in ef.References
+                                           where a.idReference.ToString() == id.ToString()
+                                           select a.descReference).ToArray()[0];
+            
+               integrantesClan.Items.Add(parte_intermedia);
+            }
+
+        }
+
+        private void RellenarListBoxMedio()
+        {
+            IEnumerable<string> producto_intermedio_ids = from b in ef.Structure.AsEnumerable()
+                                                          where b.idReferenceFinal.ToString() == idEscandallo
+                                                          select b.idReferencePart.ToString();
+
+            foreach (string id in producto_intermedio_ids)
+            {
+                string parte_intermedia = (from a in ef.References
+                                           where a.idReference.ToString() == id.ToString()
+                                           select a.descReference).ToArray()[0];
+
+                integrantesClan.Items.Add(parte_intermedia);
+            }
+
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             this.Hide();
